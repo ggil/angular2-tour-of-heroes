503546564fa4f5bfdde9ca40e952ddce21c17107
Gustavo Gil <ggil@gctech.com.ve>
2016-01-31 13:31:28 -0430

    wait.. Building Super Hero

---
+++ app/app.component.ts
@@ -0,0 +1,97 @@
+// Access to X Function importing the primary angular library
+import {Component} from 'angular2/core';
+
+interface Hero {
+  id: number;
+  name: string;
+}
+
+// Apply X function Invoking it
+@Component({
+    selector: 'my-app', /* Specify CSS for host html*/
+    template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes" (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    `, /* Html Forms for render a view */
+    styles:[`
+      .selected {
+        background-color: #CFD8DC !important;
+        color: white;
+      }
+      .heroes {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 10em;
+      }
+      .heroes li {
+        cursor: pointer;
+        position: relative;
+        left: 0;
+        background-color: #EEE;
+        margin: .5em;
+        padding: .3em 0em;
+        height: 1.6em;
+        border-radius: 4px;
+      }
+      .heroes li.selected:hover {
+        color: white;
+      }
+      .heroes li:hover {
+        color: #607D8B;
+        background-color: #EEE;
+        left: .1em;
+      }
+      .heroes .text {
+        position: relative;
+        top: -3px;
+      }
+      .heroes .badge {
+        display: inline-block;
+        font-size: small;
+        color: white;
+        padding: 0.8em 0.7em 0em 0.7em;
+        background-color: #607D8B;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: .8em;
+        border-radius: 4px 0px 0px 4px;
+      }
+    `]
+})
+
+// Export file as a Component, like a Driver Class
+export class AppComponent {
+  public title = 'Tour of Heroes';
+  public hero : Hero = {
+  id: 1,
+  name: 'Windstorm'
+  };
+  public heroes = HEROES;
+}
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
---
+++ app/boot.ts
@@ -0,0 +1,8 @@
+// Access to X Function importing the angular libraries like...
+// ..Angular Browser
+import {bootstrap}    from 'angular2/platform/browser'
+// ..Applications root
+import {AppComponent} from './app.component'
+
+// Call bootstrap passing both params
+bootstrap(AppComponent);
